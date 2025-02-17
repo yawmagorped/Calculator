a7fc40f40b758aacd7970895d0cc0fbc4a8cd82e
ali <193356044+yawmagorped@users.noreply.github.com>
2025-02-17 23:19:02 +0330

    display buttons using javascript

--- scripts.js
+++ scripts.js
@@ -30,3 +30,27 @@ function operate(a, b, op) {
             return divide(a, b);
     }
 }
+
+const mainBtnContainer = document.querySelector(".mainBtnContainer");
+
+mainBtnContainer.appendChild(CreateButton('0'));
+mainBtnContainer.appendChild(CreateButton('.'));
+mainBtnContainer.appendChild(CreateButton('='));
+
+for (let i = 1; i < 10; i++) {
+    mainBtnContainer.appendChild(CreateButton(i));
+}
+
+const sideBtnContainer = document.querySelector(".sideBtnContainer");
+let operatorsList = ['/', '*', '-', '+']
+
+for (let i = 0; i < 4; i++) {
+    sideBtnContainer.appendChild(CreateButton(operatorsList[i]));
+}
+
+function CreateButton(id) {
+    let button = document.createElement("button");
+    button.id = id.toString();
+    button.textContent = id.toString();
+    return button;
+}
