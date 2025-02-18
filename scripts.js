8f92894e10c5dadabf1c30d672d0d3ccbf8f75ec
ali <193356044+yawmagorped@users.noreply.github.com>
2025-02-18 06:49:18 +0330

    adding logic

--- scripts.js
+++ scripts.js
@@ -11,26 +11,6 @@ function divide(a, b) {
     return a / b;
 }
 
-let firstOperand;
-let operator;
-let secondOperand;
-
-function operate(a, b, op) {
-    switch (op) {
-        case '+':
-            return add(a, b);
-            break;
-        case '-':
-            return substract(a, b);
-            break;
-        case '*':
-            return multiply(a, b);
-            break;
-        case '/':
-            return divide(a, b);
-    }
-}
-
 const mainBtnContainer = document.querySelector(".mainBtnContainer");
 
 mainBtnContainer.appendChild(CreateButton('0'));
@@ -53,4 +33,70 @@ function CreateButton(id) {
     button.id = id.toString();
     button.textContent = id.toString();
     return button;
+}
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => getInput(e.target.id));
+});
+
+let firstOperand = null;
+let operator;
+let secondOperand = null;
+let numberStr = '';
+
+function getInput(id) {
+    switch (id) {
+        case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '0': case '.':
+            addOperator(id);
+            break;
+        case '+': case '-': case '/': case '*':
+            addOperand(id);
+            break;
+        case '=':
+            break;
+        break;
+    }
+}
+
+function addOperator(id) {
+    if (numberStr.includes('.') && id == '.') return;
+    numberStr += id;
+    display.textContent = numberStr;
+}
+
+function addOperand(id) {
+    operator = id;
+    if(firstOperand == null) {
+        display.textContent = operator;
+        firstOperand = parseFloat(numberStr);
+    }   else {
+        if(numberStr != '')
+            secondOperand = parseFloat(numberStr);
+
+        firstOperand = evaluate(firstOperand, secondOperand, operator);
+        secondOperand = firstOperand;
+        display.textContent = firstOperand;
+    }
+    numberStr = '';
+}
+
+
+function evaluate(a, b, op) {
+    switch (op) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return substract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+    }
 }
